4d1d6cc177c17cba1a12d38797a5da94ecdb55ae
Ryan <kerneltux0@gmail.com>
2019-06-12 10:23:18 -0600

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -3,7 +3,29 @@ require 'pry'
 
 class PigLatinizer
 
-  
+  def piglatinize(text)
+    @text = text
+    array = []
+
+    word_phrase = @text.split(' ')
+    word_phrase.each do |words|
+      if words.start_with?(/[bcdfghjklmnpqrstvwxyz]/i) == true
+        new_word = words.split(/([aeiou].*)/)
+        new_text = new_word[1]
+        new_text << new_word[0]
+        new_text << "ay"
+        array << new_text
+        @text = array.join(' ')
+      else
+        words << "way"
+        array << words
+        @text = array.join(' ')
+      end
+
+    end
+    @text
+
+  end
 
 
 end
